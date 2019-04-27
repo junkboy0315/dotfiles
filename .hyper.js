16b9e8e32fca5456e55e8573489df0d0f1c8c9a0
Shota Tamura <tamura@yuuniworks.com>
2019-04-27 21:09:01 +0900

    bigger font for hyper.js

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 15,
 
     // font family with optional fallbacks
     fontFamily:
