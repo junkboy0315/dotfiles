40f4a0b69675397c30b7860b449dfbb1a82a9f3a
Shota Tamura <tamura@yuuniworks.com>
2020-11-20 17:19:11 +0900

    remove mypy

--- setup.sh
+++ setup.sh
@@ -22,14 +22,3 @@ if [ ! -e ~/.config/karabiner ]; then
 else
   echo "karabiner config folder already exists" >&2
 fi
-
-#
-# mypy
-#
-
-if [ ! -e ~/.config/mypy ]; then
-  script_dir=$(cd $(dirname $0); pwd)
-  ln -s "$script_dir/mypy" ~/.config/mypy
-else
-  echo "mypy config folder already exists" >&2
-fi
